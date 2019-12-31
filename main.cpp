286d42f2031625fbd821aaaac8c98b830f069cb9
Alejandro <chuyaz05@gmail.com>
2019-12-30 22:24:40 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::Music music;
+    if(!music.openFromFile("newIntro.ogg")){
+            return -1;
+    }
+    music.play();
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+
+    }
+
+    return 0;
+}
